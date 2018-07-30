f712d3dcb54eb64a7cfce03279f7caa63393c39e
raheel1633 <raheel1633@gmail.com>
2018-07-30 22:52:01 +0500

    Updates

---
+++ NetCore.API/Helpers/Extensions.cs
@@ -0,0 +1,7 @@
+namespace NetCore.API.Helpers
+{
+    public class Extensions
+    {
+        
+    }
+}
--- NetCore.API/Startup.cs
+++ NetCore.API/Startup.cs
@@ -14,6 +14,9 @@ using NetCore.API.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
 
 namespace NetCore.API
 {
@@ -47,7 +50,6 @@ namespace NetCore.API
                               ValidateAudience = false
                        };
                     });
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +58,21 @@ namespace NetCore.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else // this code for golbal level exception handling
+            {
+                app.UseExceptionHandler(builder => {
+
+                    builder.Run(async context => {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error != null)
+                        {
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
             }
             app.UseCors(c=> c.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication(); // it is use for Authentication which we implement
